ec1a86a9e459a26fcd9d2d7bb1c12062366ad1ad
danielequals <d.gouldman@equalsmoney.com>
2025-07-02 17:04:27 +0100

    Delete prototype-builds directory
    to be remade

--- prototype-builds/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { OnboardingFlow } from "./screens/OnboardingFlow";
-
-createRoot(document.getElementById("app") as HTMLElement).render(
-  <StrictMode>
-    <OnboardingFlow />
-  </StrictMode>,
-);
--- prototype-builds/src/screens/OnboardingFlow/index.ts
+++
@@ -1,1 +0,0 @@
-export { OnboardingFlow } from './OnboardingFlow';
